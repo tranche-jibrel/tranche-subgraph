35190260e0cdaff0523dad6cd2f52b13d9173ad2
hg-tranche <70196848+hg-tranche@users.noreply.github.com>
2022-02-11 14:21:34 +0400

    testing and fixes of tranche sub graph

--- tranche-protocol/src/mapping.ts
+++ tranche-protocol/src/mapping.ts
@@ -1,27 +1,33 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   TrancheAddedToProtocol, TrancheATokenMinted, TrancheBTokenMinted,
-  TrancheATokenRedemption, TrancheBTokenRedemption
+  TrancheATokenRedemption, TrancheBTokenRedemption, Tranche as TrancheContract
 } from "../generated/Tranche/Tranche"
 import { Tranche, TrancheUser } from "../generated/schema"
 
 export function handleTrancheAdd(event: TrancheAddedToProtocol): void {
-  let { trancheNum, trancheA, trancheB } = event.params;
+  let trancheNum = event.params.trancheNum;
+  let trancheA = event.params.trancheA;
+  let trancheB = event.params.trancheB
   let trancheObj = Tranche.load(trancheNum.toHex());
   if (trancheObj == null) {
     trancheObj = newTranche(trancheNum.toString(), trancheA.toHex(), trancheB.toHex())
   }
+  // let trancheContract = TrancheContract.bind(event.address);
+
   trancheObj.save();
 }
 
 export function handleBuyTrancheA(event: TrancheATokenMinted): void {
-  let { trancheNum, buyer, taAmount } = event.params;
-  let userId = trancheNum + '-' + buyer.toHex()
+  let trancheNum = event.params.trancheNum;
+  let buyer = event.params.buyer;
+  let taAmount = event.params.taAmount
+  let userId = getUserId(trancheNum.toString(), buyer.toHex().toString())
   let trancheUserObj = TrancheUser.load(userId);
   if (trancheUserObj == null) {
     trancheUserObj = new TrancheUser(userId);
     trancheUserObj.address = buyer.toHex();
-    trancheUserObj.trancheNum = +trancheNum;
+    trancheUserObj.trancheNum = trancheNum.toI32();
     trancheUserObj.trancheAbalance = BigInt.fromI32(0)
     trancheUserObj.trancheBbalance = BigInt.fromI32(0)
   }
@@ -30,13 +36,15 @@ export function handleBuyTrancheA(event: TrancheATokenMinted): void {
 }
 
 export function handleBuyTrancheB(event: TrancheBTokenMinted): void {
-  let { trancheNum, buyer, tbAmount } = event.params;
-  let userId = trancheNum + '-' + buyer.toHex()
+  let trancheNum = event.params.trancheNum;
+  let buyer = event.params.buyer;
+  let tbAmount = event.params.tbAmount
+  let userId = getUserId(trancheNum.toString(), buyer.toHex().toString())
   let trancheUserObj = TrancheUser.load(userId);
   if (trancheUserObj == null) {
     trancheUserObj = new TrancheUser(userId);
     trancheUserObj.address = buyer.toHex();
-    trancheUserObj.trancheNum = +trancheNum;
+    trancheUserObj.trancheNum = trancheNum.toI32();
     trancheUserObj.trancheAbalance = BigInt.fromI32(0)
     trancheUserObj.trancheBbalance = BigInt.fromI32(0)
   }
@@ -45,13 +53,15 @@ export function handleBuyTrancheB(event: TrancheBTokenMinted): void {
 }
 
 export function handleSellTrancheA(event: TrancheATokenRedemption): void {
-  let { trancheNum, burner, amount } = event.params;
-  let userId = trancheNum + '-' + burner.toHex()
+  let trancheNum = event.params.trancheNum;
+  let burner = event.params.burner;
+  let amount = event.params.amount
+  let userId = getUserId(trancheNum.toString(), burner.toHex().toString())
   let trancheUserObj = TrancheUser.load(userId);
   if (trancheUserObj == null) {
     trancheUserObj = new TrancheUser(userId);
     trancheUserObj.address = burner.toHex();
-    trancheUserObj.trancheNum = +trancheNum;
+    trancheUserObj.trancheNum = trancheNum.toI32();
     trancheUserObj.trancheAbalance = BigInt.fromI32(0)
     trancheUserObj.trancheBbalance = BigInt.fromI32(0)
   }
@@ -60,18 +70,24 @@ export function handleSellTrancheA(event: TrancheATokenRedemption): void {
 }
 
 export function handleSellTrancheB(event: TrancheBTokenRedemption): void {
-  let { trancheNum, burner, amount } = event.params;
-  let userId = trancheNum + '-' + burner.toHex()
+  let trancheNum = event.params.trancheNum;
+  let burner = event.params.burner;
+  let amount = event.params.amount
+  let userId = getUserId(trancheNum.toString(), burner.toHex().toString())
   let trancheUserObj = TrancheUser.load(userId);
   if (trancheUserObj == null) {
     trancheUserObj = new TrancheUser(userId);
     trancheUserObj.address = burner.toHex();
-    trancheUserObj.trancheNum = +trancheNum;
+    trancheUserObj.trancheNum = trancheNum.toI32();
     trancheUserObj.trancheAbalance = BigInt.fromI32(0)
     trancheUserObj.trancheBbalance = BigInt.fromI32(0)
   }
   trancheUserObj.trancheBbalance = trancheUserObj.trancheBbalance.minus(amount)
   trancheUserObj.save();
+}
+
+function getUserId(trancheNum: string, user: string): string {
+  return trancheNum + '-' + user;
 }
 
 function newTranche(id: string, trancheAAddress: string, trancheBAddress: string): Tranche {
